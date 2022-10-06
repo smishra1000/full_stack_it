28ea9f523bb5bd67d83e89bdce8398aebcbfcd20
sameer.mishra <Sameer.Mishra@cantire.com>
2022-10-06 21:03:11 +0530

    js code added ;body background added

---
+++ script.js
@@ -0,0 +1,3 @@
+document.getElementById("box1").innerHTML="box 1 demo by div and javascript"
+document.getElementById("box1").style.color="red";
+document.getElementById("box1").style.background="cyan"
